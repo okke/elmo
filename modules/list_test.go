d5ede5e6020fbd411306be01528936318666786b
Okke <oftewel@gmail.com>
2016-07-10 13:10:35 +0200

    added unit tests for list append

--- modules/list_test.go
+++ modules/list_test.go
@@ -19,4 +19,16 @@ func TestAppend(t *testing.T) {
     l: (list 1 2 3)
     l: (el.append (l) 4)
 		l`, elmo.ExpectValue(t, elmo.ParseAndRun(elmo.NewGlobalContext(), "list 1 2 3 4")))
+
+	elmo.ParseTestAndRunBlockWithinContext(t, listContext(),
+		`el: (load "el")
+    l1: (list 1 2 3)
+    l2: (el.append (l1) 4)
+		l1`, elmo.ExpectValue(t, elmo.ParseAndRun(elmo.NewGlobalContext(), "list 1 2 3")))
+
+	elmo.ParseTestAndRunBlockWithinContext(t, listContext(),
+		`el: (load "el")
+    l: (list 1 2 3)
+    l: (el.append (l) 4 5 6)
+		l`, elmo.ExpectValue(t, elmo.ParseAndRun(elmo.NewGlobalContext(), "list 1 2 3 4 5 6")))
 }
