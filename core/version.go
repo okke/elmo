93545a829506788466911de5fd12301d1166f4c8
Okke <oftewel@gmail.com>
2016-09-12 23:04:20 +0200

    initial command line tool

---
+++ core/version.go
@@ -0,0 +1,26 @@
+package elmo
+
+import "fmt"
+
+// version info, got the idea from: https://ariejan.net/2015/10/12/building-golang-cli-tools-update/
+//
+type version struct {
+	Major int
+	Minor int
+	Patch int
+	Label string
+	Name  string
+}
+
+// Version of elmo
+var Version = version{0, 1, 0, "dev", "Chipotle"}
+
+// Git Build info
+var Build string
+
+func (v version) String() string {
+	if v.Label != "" {
+		return fmt.Sprintf("Elmo version %d.%d.%d-%s \"%s\" (git commit hash %s)", v.Major, v.Minor, v.Patch, v.Label, v.Name, Build)
+	}
+	return fmt.Sprintf("Elmo version %d.%d.%d \"%s\" (git commit hash %s)", v.Major, v.Minor, v.Patch, v.Name, Build)
+}
---
+++ tools/elmo/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/okke/elmo/core"
+	"github.com/okke/elmo/modules/dictionary"
+	"github.com/okke/elmo/modules/list"
+)
+
+func createMainContext() elmo.RunContext {
+	context := elmo.NewGlobalContext()
+
+	context.RegisterModule(el.Module)
+	context.RegisterModule(ed.Module)
+
+	// provide an exit function so the repl can be stoppped
+	// (TODO 12sep2016: should it be here?)
+	//
+	context.SetNamed(exit())
+
+	return context
+}
+
+var mainContext = createMainContext()
+
+func exit() elmo.NamedValue {
+	return elmo.NewGoFunction("exit", func(context elmo.RunContext, arguments []elmo.Argument) elmo.Value {
+		os.Exit(0)
+		return elmo.Nothing
+	})
+}
+
+func help() {
+
+	// flag.printDefault also prints test flags so
+	// let's print something useful ourselves
+	//
+	fmt.Printf("usage: elmo <flags>? <source>?\n")
+	flag.VisitAll(func(f *flag.Flag) {
+		if strings.HasPrefix(f.Name, "test.") {
+			return
+		}
+		fmt.Printf(" -%s : %s\n", f.Name, f.Usage)
+	})
+}
+
+func repl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("e>mo: ")
+	for scanner.Scan() {
+		fmt.Printf("%v\ne>mo: ", elmo.ParseAndRun(mainContext, scanner.Text()))
+	}
+}
+
+func read(source string) {
+	b, err := ioutil.ReadFile(source)
+	if err != nil {
+		fmt.Print(err)
+	}
+	result := elmo.ParseAndRunWithFile(mainContext, string(b), source)
+	if result.Type() == elmo.TypeError {
+		fmt.Printf("erro: %v\n", result)
+	}
+}
+
+func main() {
+
+	replPtr := flag.Bool("repl", false, "enforce REPL mode, even after reading from file")
+	versionPtr := flag.Bool("version", false, "print version info and quit")
+	helpPtr := flag.Bool("help", false, "print help text and quit")
+
+	flag.Parse()
+
+	if *helpPtr {
+		help()
+		return
+	}
+
+	if *versionPtr {
+		fmt.Printf("%v\n", elmo.Version)
+		return
+	}
+
+	if flag.NArg() == 0 {
+		// no source specified so running elmo as a REPL
+		//
+		repl()
+	} else {
+		read(flag.Args()[0])
+
+		if *replPtr {
+			repl()
+		}
+
+	}
+
+}
