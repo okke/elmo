1be474e486b49321cc892c96dccfbc255d661a49
Okke <oftewel@gmail.com>
2016-09-12 23:13:28 +0200

    build to build folder

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go build -ldflags "-X github.com/okke/elmo/core.Build=`git rev-parse HEAD`" -o elmo tools/elmo/main.go
+go build -ldflags "-X github.com/okke/elmo/core.Build=`git rev-parse HEAD`" -o build/elmo tools/elmo/main.go
